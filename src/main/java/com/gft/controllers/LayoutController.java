2ee62e2359ed01550f7f82167c41521003eb5577
assm <alessandra.cruz@gft.com>
2022-10-29 20:28:31 -0300

    iniciando pontuacao

--- src/main/java/com/gft/controllers/LayoutController.java
+++ src/main/java/com/gft/controllers/LayoutController.java
@@ -2,6 +2,7 @@ package com.gft.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,6 +12,12 @@ public class LayoutController
 @RequestMapping	
 public ModelAndView exibeLayout() {
 	ModelAndView mv = new ModelAndView("layout.html");
+	return mv;
+}
+
+@RequestMapping(method = RequestMethod.GET, path = "pontuacao")
+public ModelAndView exibePontuacao() {
+	ModelAndView mv = new ModelAndView("ranking/pontuacao.html");
 	return mv;
 }
 
